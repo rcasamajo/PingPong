89c59a37d4f95e4d2d66b7bf27db782e0d01ca74
rcasamajo <rcasamajo@iespoblenou.org>
2017-03-03 20:21:41 +0100

    Jugador.cs

---
+++ PingPong/Jugador.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPong
+{
+    class Jugador
+    {
+        public String Nom { get; set; }
+        public String Foto { get; set; }
+        public int Punts { get; set; }
+        public int Guanyats { get; set; }
+    }
+}
