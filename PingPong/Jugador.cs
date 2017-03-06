318143e3604994687b6b6745181eab54a6c5a7f7
Ramon <rcasamajo@iespoblenou.org>
2017-03-06 15:28:59 +0100

    DataContractJsonSerializer

--- PingPong/Jugador.cs
+++ PingPong/Jugador.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PingPong
 {
+    [DataContract]
     class Jugador
     {
+        [DataMember]
         public String Nom { get; set; }
+        [DataMember]
         public String Foto { get; set; }
+        [DataMember]
         public int Punts { get; set; }
-        public int Guanyats { get; set; }
+        [DataMember]
+        public int Jugats { get; set; }
+
+        public override String ToString()
+        {
+            return Nom + "/" + Foto + "/" + Punts + "/" + Jugats;
+        }
     }
 }
--- PingPong/MainForm.cs
+++ PingPong/MainForm.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Windows.Forms;
+using System.Runtime.Serialization.Json;
 
 namespace PingPong
 {
@@ -15,28 +16,33 @@ namespace PingPong
         private void testToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Create a request for the URL
-            WebRequest request = WebRequest.Create("https://pingpong-f6fb0.firebaseio.com/jugadors.json");
+            WebRequest request = WebRequest.Create("https://pingpong-f6fb0.firebaseio.com/jugadors/ID3.json");
 
             // Get the response.  
             WebResponse response = request.GetResponse();
 
             // Display the status.
-            Console.WriteLine();
             MessageBox.Show(((HttpWebResponse)response).StatusDescription);
 
             // Get the stream containing content returned by the server.  
             Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.  
-            StreamReader reader = new StreamReader(dataStream);
 
+            /*
+            //Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
             // Read the content.  
             string responseFromServer = reader.ReadToEnd();
-
             // Display the content.  
             MessageBox.Show(responseFromServer);
+            */
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Jugador));
+            Jugador p1 = (Jugador)ser.ReadObject(dataStream);
+
+            MessageBox.Show(p1.ToString());
 
             // Clean up the streams and the response.  
-            reader.Close();
+            dataStream.Close();
             response.Close();
         }
     }
