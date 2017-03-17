4505da75c799df699a85e49927f322df16cbb51f
rcasamajo <rcasamajo@iespoblenou.org>
2017-03-17 19:38:04 +0100

    Arreglat Modificar

--- PingPong/Jugador.cs
+++ PingPong/Jugador.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Runtime.Serialization;
 
 namespace PingPong
 {
     class Jugador
     {
+        [JsonIgnore] //Aquest camp no es serialitza
         public String Id { get; set; }
+        [JsonProperty("Nom")] //Si l'atribut tingués nom diferent de la propietat JSON podem fer l'associació així
         public String Nom { get; set; }
         public String Foto { get; set; }
         public int Punts { get; set; }
